f044238305f3f43c7d9532a6f4b416da7e7da566
sukzoon**** <sukzoon1234@naver.com>
2021-02-07 07:39:17 +0000

    2021.02.07

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
-
+ 
 var app = http.createServer(function(request,response){
     var _url = request.url;
-	var queryData = url.parse(_url,true);
-	var queryString=require('querystring');
-	console.log(queryData);
-	console.log(_url);
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
     if(_url == '/'){
-    	_url = '/index.html';
+      title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
-     	response.writeHead(404);
-		response.end();
-		return;
+      return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + _url));
-	//__dirname 은 현재 실행중인 폴더의 경로를 나타냄
+    fs.readFile(`data/${queryData.id}.txt`, 'utf8', function(err, description){
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+      response.end(template);
+    })
+ 
  
 });
-app.listen(3000,function(){
-	console.log("Access");
-});
+console.log('success')
+app.listen(3000);
